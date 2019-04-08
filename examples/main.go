a2a8bc0ee530f369a13178972ac964bbf6f81264
Adriano <adriano.selaviles@gmail.com>
2019-04-07 18:29:16 -0700

    latest

--- examples/main.go
+++ examples/main.go
@@ -5,18 +5,20 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/adrianosela/middleman"
 	"github.com/adrianosela/sslmgr"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
 	h := middleman.Logger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Im alive!"))
 	}))
 
 	rtr := mux.NewRouter()
-	rtr.Methods(http.MethodGet).Path("/healthcheck").Handler()
+	rtr.Methods(http.MethodGet).Path("/healthcheck").Handler(h)
 
 	ss, err := sslmgr.NewSecureServer(sslmgr.ServerConfig{
 		Hostnames: []string{os.Getenv("CN_FOR_CERTIFICATE")},
--- logger.go
+++ logger.go
@@ -1,8 +1,9 @@
-package httpmiddleware
+package middleman
 
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/teris-io/shortid"
 )
@@ -11,12 +12,45 @@ const (
 	loggerHeader = "[http]"
 )
 
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+	length int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
+func (w *statusWriter) Write(b []byte) (int, error) {
+	n, err := w.ResponseWriter.Write(b)
+	w.length += n
+	return n, err
+}
+
 // Logger Middleware logs all http requests served
 func Logger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		reqid, _ := shortid.Generate()
-		log.Printf("%s %s %s >> RECEIVED %s", loggerHeader, r.Method, r.URL.Path, reqid)
-		h.ServeHTTP(w, r)
-		log.Printf("%s %s %s << DONE %s with status %s", loggerHeader, r.Method, r.URL.Path, reqid, r.Response.Status)
+		start := time.Now().UnixNano()
+		sw := statusWriter{ResponseWriter: w}
+
+		log.Printf("%s[%s] %s %s",
+			loggerHeader,
+			reqid,
+			r.Method,
+			r.URL.Path,
+		)
+
+		h.ServeHTTP(&sw, r)
+
+		log.Printf("%s[%s] %d %s, took %d ms",
+			loggerHeader,
+			reqid,
+			sw.status,
+			http.StatusText(sw.status),
+			(time.Now().UnixNano()-start)/1000000,
+		)
 	})
 }
